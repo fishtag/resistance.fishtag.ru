7b76b6edca0be9e7b1748ad53ea85f0fc733db7d
Timur Khafizov <timur.khafizov@fishtag.ru>
2015-05-13 11:37:49 +0300

    Hotfix for User#win_percentage returning NaN for User without finished games

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ActiveRecord::Base
   end
 
   def win_percentage
+    return 0 if finished_game_sessions.size.zero?
+
     ((won_game_sessions.size / finished_game_sessions.size.to_f) * 100)
   end
 end
