31fa61d9d1b707e50e5cba2491d7c32bb3845e01
Timur Khafizov <timur.khafizov@fishtag.ru>
2015-05-13 17:25:15 +0300

    Add additional percente counting for fraction game sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,20 @@ class User < ActiveRecord::Base
   include GameSessionsAssociations
 
   def win_percentage
-    return 0 if finished_game_sessions.size.zero?
+    count_win_percentage(won_game_sessions, finished_game_sessions)
+  end
 
-    ((won_game_sessions.size / finished_game_sessions.size.to_f) * 100)
+  def spy_win_percentage
+    count_win_percentage(won_spies_game_sessions, spies_game_sessions)
+  end
+
+  def resistance_win_percentage
+    count_win_percentage(won_resistance_game_sessions, resistance_game_sessions)
+  end
+
+  def count_win_percentage(won_game_sessions, all_game_sessions, round_to = 2)
+    return 0 if all_game_sessions.size.zero?
+
+    (won_game_sessions.size / all_game_sessions.size.to_f * 100).round(round_to)
   end
 end
