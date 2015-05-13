74448448ee5bbb40da635ff473373a64e5ba2460
Timur Khafizov <timur.khafizov@fishtag.ru>
2015-05-13 17:32:42 +0300

    Hotfix GameSessionsUser method definition order

--- app/models/game_sessions_user.rb
+++ app/models/game_sessions_user.rb
@@ -1,6 +1,6 @@
 class GameSessionsUser < ActiveRecord::Base
+  enum fraction: GAME_FRACTIONS
+
   belongs_to :game_session, inverse_of: :game_sessions_users
   belongs_to :user, counter_cache: :game_sessions_count
-
-  enum fraction: GAME_FRACTIONS
 end
