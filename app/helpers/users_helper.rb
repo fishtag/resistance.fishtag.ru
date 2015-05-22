c6f1f7fcec688d44c9637ce07cd5e23950a69f4d
Timur Khafizov <timur.khafizov@fishtag.ru>
2015-05-22 10:07:56 +0300

    Sort users by multiple win percentages counting spy and resistance along with total

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def sort_users_by_wins(users)
+    users.sort_by do |user|
+      [user.win_percentage, user.spy_win_percentage, user.resistance_win_percentage]
+    end.reverse
+  end
 end
